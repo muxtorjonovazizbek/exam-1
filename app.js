620785be1d78ea0b3c9679d63b855e561998dc03
muxtorjonovazizbek <azizbekmukhtorjonov581@gmail.com>
2024-08-16 23:25:13 +0500

    done

---
+++ app.js
@@ -0,0 +1,138 @@
+const add__product = document.getElementById("addProduct")
+const user__modal = document.getElementById("user__modal")
+const result = document.getElementById("result")
+const save = document.getElementById("save")
+let form = {}
+let products = []
+let editId = null
+let baseUrl = "http://localhost:3000/product"
+
+document.addEventListener("DOMContentLoaded", () => {
+    add__product.addEventListener("click", openModal)
+    save.addEventListener("click", saveModal)
+    getProducts()
+})
+   
+
+function openModal() {
+    toggleModal("block")
+}
+
+window.addEventListener("click", (event) => {
+    if (event.target === user__modal) {
+        toggleModal("none")
+    }
+})
+
+
+function toggleModal(status) {
+    user__modal.style.display = status
+}
+
+function handleChange(event) {
+    const {name,value} = event.target
+    form = {...form, [name]:value}
+}
+
+async function saveModal() {
+if (editId) {
+    try {
+        await fetch(`${baseUrl}/${editId}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+        getProducts()
+    } catch (error) {
+        console.log(error);
+        
+    }
+}else {
+    try{
+        const response = await fetch(`${baseUrl}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+        console.log(response);
+        getProducts()
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+   
+}
+
+
+async function getProducts() {
+    try {
+        const response = await fetch(`${baseUrl}`)
+        products = await response.json()
+        displayProduct()
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+function displayProduct() {
+    result.innerHTML = ""
+    products.forEach((val, ind) => {
+        let tr = document.createElement("tr")
+        tr.innerHTML = `
+            <td>${ind + 1}</td>
+            <td>${val.name}</td>
+            <td>${val.price}</td>
+            <td>${val.brand}</td>
+            <td>${val.color}</td>
+            <td>${val.year}</td>
+            <td>
+                <button class="btn btn-primary mx-1" onclick="editProducts('${val.id}')">Edit</button>
+                <button class="btn btn-danger mx-1" onclick="delProducts('${val.id}')">Del</button>
+            </td>
+        `
+        result.appendChild(tr)
+    })
+}
+
+async function delProducts(id) {
+    console.log(id);
+    
+    try {
+        const response = await fetch(`${baseUrl}/${id}`, {
+            method: "DELETE"
+        })
+
+        if (response.ok) {
+            getProducts()
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+    async function editProducts(id) {
+        editId = id
+        let product = products.find(item => item.id == id)
+        form = {...product}
+        toggleModal("block")
+        
+        document.querySelector("input[name='name']").value  = form.name
+        document.querySelector("input[name='price']").value  = form.price
+        document.querySelector("input[name='brand']").value  = form.brand
+        document.querySelector("input[name='color']").value = form.color
+        document.querySelector("input[name='year']").value  = form.year
+        
+    }
+    
+
+
